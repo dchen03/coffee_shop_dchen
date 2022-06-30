41b1d0b30461313c1235cbdd25b7854e78f98df9
Ubuntu <ubuntu@ip-172-31-16-133.us-east-2.compute.internal>
2022-06-30 02:12:04 +0000

    troubleshootingdbt doesnt run

--- models/marts/engineering/pull_requests.sql
+++ models/marts/engineering/pull_requests.sql
@@ -1,48 +1,52 @@
-with 
 
-pull_requests
+with
 
-repositories
+pull_requests as (
+    select * from {{ ref('stg_github__pull_requests') }}
+),
 
-issues
+repositories as (
+    select * from {{ ref('stg_github__repositories') }}
+),
 
-issues_merged
+issues as (
+    select * from {{ ref('stg_github__issues') }}
+),
 
-
+issues_merged as (
+    select * from {{ ref('stg_github__issues_merged') }}
+),
 
 final as (
-
-    select 
+    select
         pull_requests.pull_request_id,
-        repository.name as repo_name, 
+        repositories.name as repo_name,
         issues.number as pull_request_number,
 
-        -- TODO: find out how to label these PRs , get state
+        -- TODO: find out how to label these PRs
         cast(null as string) as type, -- (bug, eng, feature),
 
-        case 
+        case
             when pull_requests.is_draft then 'draft'
-            when issue_merged.merged_at is not null then 'merged'
-            when issue_merged.closed_at is not null then 'closed_without_merge'
+            when issues_merged.merged_at is not null then 'merged'
+            when issues.closed_at is not null then 'closed_without_merge'
             else 'open'
         end as state,
-        
-        issues.created_at as opened_at,
-        issue_merged.merged_at,
-        date_diff(issues.created_at, issue_merged.merged_at, hour)/ 24.0 as days_open_to_merge
 
+        issues.created_at as opened_at,
+        issues_merged.merged_at,
+        round(date_diff(issues_merged.merged_at, issues.created_at, hour) / 24.0, 2) as days_open_to_merge
 
     from pull_requests
-    left join repositories
-        on pull_requests.head_repo_id = repositories.repository_id
 
-    left join issues 
+    left join repositories
+        on pull_requests.head_repo_id = repositories.repo_id
+
+    left join issues
         on pull_requests.issue_id = issues.issue_id
 
-    left join issue_merged
-        on pull_requests.issue_id = issue_merged.issue_id
-
-
+    left join issues_merged
+        on pull_requests.issue_id = issues_merged.issue_id
 )
 
 select * from final
