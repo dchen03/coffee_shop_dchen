0d32fb6541591d4e73cda13f2a21922e6b72470d
Ubuntu <ubuntu@ip-172-31-16-133.us-east-2.compute.internal>
2022-06-30 06:40:37 +0000

    moved original customers.sql and monthly_customers.sql models into marts, refactored code with stg_ models

---
+++ models/marts/product/dim_customers.sql
@@ -0,0 +1,24 @@
+{{ config(
+    materialized='table'
+) }}
+
+with customer_orders as (
+    select
+          customer_id, 
+          count(*) as n_orders, 
+          min(order_created_at) as first_order_at
+    from {{ ref('stg_coffee_shop__orders') }} as orders
+    group by customer_id
+)
+
+select 
+     customers.customer_id,
+     customers.customer_name,
+     customers.customer_email,
+     coalesce(customer_orders.n_orders, 0) as n_orders,
+     customer_orders.first_order_at 
+--- use customers as base to get customers with no orders
+from {{ ref('stg_coffee_shop__customers') }} as customers
+left join  customer_orders
+  on  customers.customer_id = customer_orders.customer_id 
+order by customer_orders.first_order_at 
