e0c18b4b8be9237b047781e44bd534cf862103ca
Ubuntu <ubuntu@ip-172-31-16-133.us-east-2.compute.internal>
2022-07-07 20:25:22 +0000

    create new column for custome_are paired to visitor_id

--- models/marts/product/user_stitching.sql
+++ models/marts/product/user_stitching.sql
@@ -4,22 +4,30 @@ pageviews as (
     select * from {{ ref('stg_web_tracking__pageviews') }}
 ),
 
-final as (
+-- customer id and visitor id crosswalk 
+customer_visitor as ( 
+    select distinct 
+        customer_id, 
+        visitor_id
+    from pageviews
+    where customer_id is not null
+    order by customer_id
+),
+
+--bring in customer_id for every known visitor as customer_visitor_id
+user_stitching as (
     select
         pageviews_id,
-        visitor_id,
-        customer_id,
+        pageviews.visitor_id,
+        pageviews.customer_id,
+        customer_visitor.customer_id as customer_visitor_id,
         device_type,
         page,
         pageview_at,
-        case
-            when customer_id is not null then concat('a', dense_rank() over (order by customer_id))
-            else null
-        end as new_visitor_id
     from pageviews
-    order by
-        customer_id
-
+    left join customer_visitor
+        on pageviews.visitor_id = customer_visitor.visitor_id
 )
 
-select * from final
+select * from user_stitching
+
