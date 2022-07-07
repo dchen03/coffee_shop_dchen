370521d0bb26cb4fe845b0d21126f1a88ef754ae
Ubuntu <ubuntu@ip-172-31-16-133.us-east-2.compute.internal>
2022-07-07 21:43:35 +0000

    add sessionization, requires testing and iteration

--- models/marts/product/user_stitching.sql
+++ models/marts/product/user_stitching.sql
@@ -4,6 +4,8 @@ pageviews as (
     select * from {{ ref('stg_web_tracking__pageviews') }}
 ),
 
+/* User stitching */
+
 -- customer id and visitor id crosswalk 
 customer_visitor as ( 
     select distinct 
@@ -27,7 +29,34 @@ user_stitching as (
     from pageviews
     left join customer_visitor
         on pageviews.visitor_id = customer_visitor.visitor_id
+),
+
+/* Sessionization */
+
+col_after_30 as (
+    select *,
+        timestamp_diff(pageview_at, lag(pageview_at) over (partition by customer_id order by pageview_at), MINUTE) > 30 as after_30
+    from user_stitching
+    order by customer_id, pageview_at
+),
+
+col_new_session as (
+    select *, 
+        case 
+            when after_30 is null then 1 
+            when after_30 then 1
+            else 0
+        end as is_new_session
+    from col_after_30
+),
+
+final as (
+    select *,
+        sum(is_new_session) over (order by customer_id, pageview_at) as global_session_id,
+        sum(is_new_session) over (partition by customer_id order by pageview_at) as user_session_id
+    from col_new_session
 )
 
-select * from user_stitching
+select * from final 
+
 
