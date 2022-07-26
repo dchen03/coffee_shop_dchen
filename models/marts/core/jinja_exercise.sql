a647aa90104579a982bc3a9240eec5d828db04ce
Ubuntu <ubuntu@ip-172-31-16-133.us-east-2.compute.internal>
2022-07-26 02:33:38 +0000

    add another way to append _amount to string

--- models/marts/core/jinja_exercise.sql
+++ models/marts/core/jinja_exercise.sql
@@ -6,7 +6,8 @@
 select
   date_trunc(order_created_at, month) as date_month,
   {%- for product_category in product_categories %}
-  sum(case when product_category = '{{ product_category }}' then product_price end) as {{ product_category | replace(" ", "_") + "_amount" }}
+ -- sum(case when product_category = '{{ product_category }}' then product_price end) as {{ product_category | replace(" ", "_") + "_amount" }}
+  sum(case when product_category = '{{ product_category }}' then product_price end) as {{ product_category | replace(" ", "_") }}_amount
   {%- if not loop.last %},{% endif -%}
   {% endfor %}
 
